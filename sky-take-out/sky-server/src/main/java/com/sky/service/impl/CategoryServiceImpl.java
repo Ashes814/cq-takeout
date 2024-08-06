2633fa4885ea3a8e18c2b9c64e62349aaaa9d016
Ashes814 <1534661514@qq.com>
2024-08-06 09:46:48 +0800

    update add new category

---
+++ sky-take-out/sky-server/src/main/java/com/sky/service/impl/CategoryServiceImpl.java
@@ -0,0 +1,39 @@
+package com.sky.service.impl;
+
+
+import com.sky.constant.StatusConstant;
+import com.sky.context.BaseContext;
+import com.sky.dto.CategoryDTO;
+import com.sky.entity.Category;
+import com.sky.mapper.CategoryMapper;
+import com.sky.service.CategoryService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+
+@Service
+@Slf4j
+public class CategoryServiceImpl implements CategoryService {
+    @Autowired
+    private CategoryMapper categoryMapper;
+    /**
+     * Save new category
+     * @param categoryDTO
+     */
+    public void save(CategoryDTO categoryDTO) {
+        Category category = new Category();
+        BeanUtils.copyProperties(categoryDTO, category);
+        category.setCreateTime(LocalDateTime.now());
+        category.setCreateUser(BaseContext.getCurrentId());
+        category.setUpdateTime(LocalDateTime.now());
+        category.setUpdateUser(BaseContext.getCurrentId());
+        category.setStatus(StatusConstant.DISABLE);
+        categoryMapper.insert(category);
+
+
+
+    }
+}
